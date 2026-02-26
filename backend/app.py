55ad782343680857f080f9582b5012f95ff09ca9
Abhay Maurya <abhaymaurya821@gmail.com>
2026-02-26 17:44:51 +0530

    create the virutal env

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/submittodoitem", methods=["POST"])
+def submit_todo():
+    data = request.json
+    itemName = data.get("itemName")
+    itemDescription = data.get("itemDescription")
+
+    print("Received:", itemName, itemDescription)
+
+    return jsonify({"message": "Item Stored Successfully"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
