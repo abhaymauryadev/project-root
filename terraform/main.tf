3d0b595ca16f12d36c34111ff674a071038fc518
Abhay Maurya <abhaymaurya821@gmail.com>
2026-04-09 17:45:15 +0530

    fix security group and terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,51 +1,35 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "fullstack" {
-  ami = "ami-0ec10929233384c7f" # ubuntu
-  instance_type = "t2.micro"
-
-  key_name = var.key_name
-  
-  security_groups = [aws_security_group.web_sg.name]
-
-  user_data = file("userdata.sh")
-
-  tags = {
-    Name = "Fullstack_EC2"
-  }
-}
-
 resource "aws_security_group" "web_sg" {
   name = "web_sg"
 
-  ingress = {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress = {
-     from_port = 3000
-    to_port = 3000
-    protocol = "tcp"
+  # Frontend (React/Next)
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-   ingress = {
-     from_port = 5000
-    to_port = 5000
-    protocol = "tcp"
+  # Backend (Flask)
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Outbound (VERY IMPORTANT — this is egress, not ingress)
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
